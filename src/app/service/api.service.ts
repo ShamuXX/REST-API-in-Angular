287a7684b741a6baabadbc7f3982a59433e786a0
Samuel Mendoza <samuel16mendoza@gmail.com>
2024-01-20 01:10:05 -0600

    apiService was created
    Use the `getData` method in the web application to retrieve the data from the api.

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private urlAPI = 'https://jsonplaceholder.typicode.com/posts'
+  constructor(private http: HttpClient) { }
+
+  public getData() : Observable<any> {
+    return this.http.get<any>(this.urlAPI);
+  }
+}
