5d52040880ea9efd3bf29d3097d91e08051ffb29
Samuel Mendoza <samuel16mendoza@gmail.com>
2024-01-20 01:15:47 -0600

    use ApiService to get Data
    TakeData obtains the data through the apiService service and assigns the elements containing the information in the form of arrays.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
+  moduleId: module.id,
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit{
+  data: any[] = [];
+  constructor(private apiService: ApiService) {}
+  
+  ngOnInit(): void {
+    this.takeData();
+  }
+  
+  takeData( ){
+    this.apiService.getData().subscribe(data => {
+      this.data = data;
+    })
+  }
 }
